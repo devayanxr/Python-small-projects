25734e1b9933e36c224d8104c7b0a3914a77b43c
DEVAYAN DAS <devayandas884@gmail.com>
2025-01-01 18:34:55 +0530

    Add files via upload

---
+++ Addition.py
@@ -0,0 +1,6 @@
+#-*- coding: utf-8 -*-
+
+first_number = float(input("Enter your First Number: "))
+second_number = float(input("Enter your Second Number: "))
+addition = round(first_number + second_number,2) 
+print(f"The sum of the numbers is: {addition}")
